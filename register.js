77628479cf18257d8a2eea52fafd6233fbde89a3
yvandylan rafanan <rafanandylan@gmail.com>
2024-03-05 14:31:21 +0800

    Added a password and confirm password input field

--- register.js
+++ register.js
@@ -7,12 +7,27 @@
 //console.log(typeof(name))
 
 let txtUsername = document.querySelector("#txtUsername")
+let txtPassword = document.querySelector("#txtPassword")
+let txtConfirmPassword = document.querySelector("#txtConfirmPassword")
 let btnRegister = document.querySelector("#btnRegister")
 
 btnRegister.onclick = function () {
-    register(txtUsername.value)
+    register(txtUsername.value, txtPassword.value, txtConfirmPassword.value)
 }
 
-function register (username) {
-    console.log(username)
+
+function register (username, password, confirmpassword){
+
+    console.log(username, password, confirmpassword)
+
+    if (password == confirmpassword && password != "" && confirmpassword != "" ) {
+        return console.log("Password Match!")
+
+    } else if (password == "" || confirmpassword == "") {
+        return console.log("Password must not be empty")
+        
+    } else {
+        return console.log("Password does not match.")
+    }
+    
 }
